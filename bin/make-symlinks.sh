f8be75e46ae5b59904aac699b14a97450196f182
Chris Kirkland <kirklandcm@gmail.com>
2016-09-12 14:07:03 -0500

    Added Dockerfile for testing install script (#3)
    * Added dockerfile for testing purposes
    
    * Fixed Dockerfile for Ubuntu/CentOS
    
    * Made installation general to OSX/Linux
    
    * Updated install scripts to make backing up files more resilient
    
    * Refactoring + made 2-space tabs default for all filetypes
    
    * Made setting OS specific commands more general

--- bin/make-symlinks.sh
+++ bin/make-symlinks.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
+# Works for installing dotfiles on Ubuntu, CentOS, Fedora, and OSX. Instllation
+# on OSX requires installation of `coreutils`:
+#   brew install coreutils
+if [ `uname` == "Darwin" ]
+then
+  READLINK=greadlink
+else
+  READLINK=readlink
+fi
+
+# Tries to backup $SRC file to $TARGET. Returns whether backup was successful.
+try_backup() {
+  SRC=$1
+  TARGET=$2
+
+  if [ -e $TARGET ] && diff $SRC $TARGET;
+  then
+    return false
+  else
+    mv $SRC $TARGET
+    return true
+  fi
+}
 
 # config
 PWD=`pwd`
-HOME=$(readlink -f ~)
+HOME=$($READLINK -f ~)
 FILES=(.vimrc .vim .pylintrc)
 
+# install files
 for FILE in "${FILES[@]}"
 do
   # backup file
-  if [ -e $HOME/$FILE ];
-  then
-    mv $HOME/$FILE{,.backup}
-  fi
+  TARGET=$HOME/$FILE
+  LOOP=1
+  while ! try_backup $HOME/$FILE $TARGET;
+  do
+    TARGET="$HOME/$FILE~$LOOP"
+    ((LOOP++))
+  done
 
   # link new source
   ln -s $PWD/$FILE $HOME/$FILE
